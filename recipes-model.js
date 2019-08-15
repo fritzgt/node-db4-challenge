7b4c3da96339c829745ce3c106f398b6f7da5a29
fritzgt <tuanis@live.com>
2019-08-15 09:57:33 +0300

    creating methods in the recipes-model file

---
+++ recipes-model.js
@@ -0,0 +1,11 @@
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+};
+
+function getRecipes() {}
+
+function getShoppingList(recipe_id) {}
+
+function getInstructions(recipe_id) {}
