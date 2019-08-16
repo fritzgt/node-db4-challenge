60bcc0360ac05c74fb8624a9b4005449bf40c817
fritzgt <tuanis@live.com>
2019-08-16 05:07:29 +0300

    completed getShoppingList

--- index.js
+++ index.js
@@ -6,8 +6,35 @@ server.use(express.json());
 
 const port = 7000;
 
-server.use('/', (req, res) => {
-  res.status(200).send('Hello');
+//importing db
+const Recipes = require('./recipes-model');
+
+// server.use('/', (req, res) => {
+//   res.status(200).send('Hello');
+// });
+
+server.get('/recipes', async (req, res) => {
+  try {
+    const recipes = await Recipes.getRecipes();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get schemes' });
+  }
+});
+
+server.get('/recipes/:id/ingredients', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const recipes = await Recipes.getShoppingList(id);
+    if (recipes) {
+      res.json(recipes);
+    } else {
+      res.status(404).json({ message: 'Could not find scheme with given id.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get schemes' });
+  }
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- recipes-model.js
+++ recipes-model.js
@@ -1,11 +1,26 @@
+const db = require('./db-config.js');
+
 module.exports = {
   getRecipes,
   getShoppingList,
   getInstructions
 };
 
-function getRecipes() {}
+function getRecipes() {
+  return db('recipes');
+}
 
-function getShoppingList(recipe_id) {}
+function getShoppingList(recipe_id) {
+  return db('ingredients as i')
+    .join('recipes_ingredients as r', 'i.id', '=', 'r.ingredients_id')
+    .join('recipes', 'r.recipes_id', '=', 'recipes.id')
+    .select(
+      'i.ingredient_name',
+      'r.quantity'
+      //   'r.recipes_id',
+      //   'r.ingredients_id'
+    )
+    .where('recipes_id', recipe_id);
+}
 
 function getInstructions(recipe_id) {}
